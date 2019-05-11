80789bb23ac3826a6f3d6b585421c623afa2d21e
Chase Lirette <chaselirette@yahoo.com>
2019-05-10 21:33:49 -0500

    Added cors and json

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const passport = require('passport');
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const cors = require("cors");
 
-const users = require('./api/users');
+const users = require("./api/users");
 
 const app = express();
 
-app.use(logger('dev'));
-app.use(express.urlencoded({extended: true}));
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/blog', {useNewUrlParser: true})
+mongoose
+  .connect("mongodb://localhost:27017/blog", { useNewUrlParser: true })
   .then(() => console.log("Mongo Connected"));
 
 app.use(passport.initialize());
 
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
-app.use('/api/users', users);
+app.use("/api/users", users);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
