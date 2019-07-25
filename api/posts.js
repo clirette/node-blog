a5a81232ae5f0d868fa2a7b26b9ffa3929ff5c8d
Chase Lirette <chaselirette@yahoo.com>
2019-07-24 19:42:44 -0500

    Altered api for login and current user

--- api/posts.js
+++ api/posts.js
@@ -1,16 +1,45 @@
 const router = require("express").Router();
 const passport = require("passport");
 const Post = require("../models/Post");
+const User = require("../models/User");
+
+// Helper method to strip sensitive data from either array of posts
+// or single post.
+const cleanPosts = posts => {
+  if (Array.isArray(posts)) {
+    return posts.map(post => {
+      post.createdBy.email = undefined;
+      post.createdBy.password = undefined;
+      return post;
+    });
+  } else {
+    posts.createdBy.email = undefined;
+    posts.createdBy.password = undefined;
+    return posts;
+  }
+};
+
+router.get(
+  "/my-posts",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const currentUser = await User.findById(req.user._id).populate("posts");
+    return res.send(currentUser.posts);
+  }
+);
 
 router.get(
   "/",
   passport.authenticate("jwt", { session: false }),
   async (req, res) => {
-    const posts = await Post.find({ createdBy: req.user.id }).catch(err => {
-      console.log(err);
-      return res.status(401).send(err);
-    });
-    return res.status(200).json(posts);
+    const posts = await Post.find({ createdBy: req.user.id })
+      .populate("createdBy")
+      .catch(err => {
+        console.log(err);
+        return res.status(401).send(err);
+      });
+    const cleanedPosts = cleanPosts(posts);
+    return res.status(200).json(cleanedPosts);
   }
 );
 
@@ -25,24 +54,39 @@ router.post(
       createdAt: new Date(),
       createdBy: req.user.id
     });
-    console.log(newPost);
     await newPost.save().catch(err => {
       console.log(err);
       return res.status(400).send(err);
     });
+    req.user.posts.push(newPost);
+    await req.user.save();
     return res.status(200).send(newPost);
   }
 );
 
+router.get("/all", async (req, res) => {
+  const allPosts = await Post.find({})
+    .populate("createdBy")
+    .catch(err => {
+      console.log(err);
+      return res.status(400).send(err);
+    });
+  const cleanedPosts = cleanPosts(allPosts);
+  return res.json(cleanedPosts);
+});
+
 router.get("/:id", async (req, res) => {
-  const post = await Post.findById(req.params.id).catch(err => {
-    console.log(err);
-    return res.status(400).send(err);
-  });
+  const post = await Post.findById(req.params.id)
+    .populate("createdBy")
+    .catch(err => {
+      console.log(err);
+      return res.status(400).send(err);
+    });
   if (!post) {
     return res.status(404).json({ msg: "Post not found" });
   }
-  return res.status(200).json(post);
+  const cleanPost = cleanPosts(post);
+  return res.status(200).json(cleanPost);
 });
 
 router.put(
@@ -93,16 +137,8 @@ router.delete(
         .json({ msg: "Not allowed to delete another user's post" });
     }
     await post.remove();
-    return res.status(200).json({ msg: "Post deleted" });
+    return res.status(200).json({ msg: "Post deleted", postId: req.params.id });
   }
 );
-
-router.get("/all", async (req, res) => {
-  const allPosts = await Post.find({}).catch(err => {
-    console.log(err);
-    return res.status(400).send(err);
-  });
-  return res.json(allPosts);
-});
 
 module.exports = router;
--- api/users.js
+++ api/users.js
@@ -1,65 +1,62 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-const User = require('../models/User');
-const gravatar = require('gravatar');
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
-const secret = require('../config/keys').secret;
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+const gravatar = require("gravatar");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const secret = require("../config/keys").secret;
 
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
   try {
-    const user = await User.findOne({ email })
+    const user = await User.findOne({ email });
     if (user) {
-      return res.status(400).send({msg: 'Email already registered' });
+      return res.status(400).send({ msg: "Email already registered" });
     }
     const avatar = gravatar.url(email, {
-      s: '200', //size
-      r: 'g', //rating
-      d: 'mm' //default
-    })
+      s: "200", //size
+      r: "g", //rating
+      d: "mm" //default
+    });
     const newUser = new User({ firstName, lastName, avatar, email });
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
     newUser.password = hash;
     const savedUser = await newUser.save();
     return res.json(savedUser);
-  } catch(err) {
+  } catch (err) {
     console.log(err);
-    return res.status(400).send({err});
+    return res.status(400).send({ err });
   }
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).send({ msg: 'User not found' });
+      return res.status(400).send({ msg: "User not found" });
     }
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).send({ msg: 'Incorrect password' });
+      return res
+        .status(400)
+        .send({ msg: "Incorrect password", success: false });
     }
     const payload = {
       id: user.id,
       name: `${user.firstName} ${user.lastName}`,
       avatar: user.avatar
-    }
-    const token = await jwt.sign(payload, secret, { expiresIn: '1h' });
+    };
+    const token = await jwt.sign(payload, secret, { expiresIn: "1h" });
     return res.json({
       success: true,
       token: `Bearer ${token}`
-    })
-
-  } catch(err) {
+    });
+  } catch (err) {
     console.log(err);
     return res.status(400).send(err);
   }
 });
-
-router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
-  return res.send(req.user);
-})
 
 module.exports = router;
